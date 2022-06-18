5a35d3d7001de00cfff2e52f9fe736389666dc2d
Craig <indigomx9@gmail.com>
2022-06-17 23:32:40 -0400

    Initial setup of the project.

---
+++ client/src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit maxime porro fugiat quas, aut quos molestiae alias deserunt omnis provident excepturi natus velit commodi consectetur voluptatibus neque? Repellat, vero molestias.</p>
+        </React.Fragment>
+    );
+};
+
+
---
+++ client/src/containers/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Dashboard = () => {
+    return (
+        <React.Fragment>
+            <h1>Dashboard</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit maxime porro fugiat quas, aut quos molestiae alias deserunt omnis provident excepturi natus velit commodi consectetur voluptatibus neque? Repellat, vero molestias.</p>
+        </React.Fragment>
+    );
+};
+
+
